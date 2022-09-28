d460566ce9f47afda92e8a14068f067f4744f0e4
bahadir.ak <bahadir.ak@COINO-PC-19>
2022-09-28 10:17:42 +0300

    Add project files.

---
+++ Udemy.BankApp.Web/Data/Interfaces/IApplicationUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Udemy.BankApp.Web.Data.Entities;
+
+namespace Udemy.BankApp.Web.Data.Interfaces
+{
+    public interface IApplicationUserRepository
+    {
+        List<ApplicationUser> GetAll();
+        ApplicationUser GetById(int id);
+        void Create(ApplicationUser user);
+    }
+}
---
+++ Udemy.BankApp.Web/Models/SendMoneyModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Udemy.BankApp.Web.Models
+{
+    public class SendMoneyModel
+    {
+        public int SenderId { get; set; }
+        public int AccountId { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
